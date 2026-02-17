23f939f5fcc595abfe51f68e7edc0f9153302c55
Jacob <jacobjordan94@live.com>
2026-02-17 13:38:45 -0600

    conditionally render strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import { createRoot } from "react-dom/client"
 import { LazyMotion, domAnimation, MotionConfig } from "motion/react"
 
 import "./index.css"
-import App from "./App.tsx"
+import _App from "./App.tsx"
+
+const App = () => import.meta.env.PROD ? <_App /> : <StrictMode><_App/></StrictMode>;
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
     <LazyMotion features={domAnimation}>
       <MotionConfig reducedMotion="user">
         <App />
       </MotionConfig>
     </LazyMotion>
-  </StrictMode>
 )
