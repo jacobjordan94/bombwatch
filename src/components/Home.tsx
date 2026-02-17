b127d7113054d9be67d646e1ad8da27e87e09db0
Jacob <jacobjordan94@live.com>
2026-02-17 14:52:52 -0600

    fix scrolling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -14,28 +14,28 @@ export default function HomePage({ data }: HomePageProps) {
     console.log(data);
     return (
         <Page className="p-0 px-1">
-            <AnimatePresence>
-                {data?.liveNow && (
-                    <div key="live">
-                        <LiveNowBadge className="fixed z-100 mt-2 ml-2" />
-                        <LiveNow show={data.liveNow} />
-                    </div>
-                )}
-                {(data?.upcoming && data.upcoming.length > 0) &&
-                    <ScrollArea key="scroll" className="h-[540px] pr-3 overflow-clip">
+            <ScrollArea className="h-[540px] pr-3 overflow-clip">
+                <AnimatePresence>
+                    {data?.liveNow && (
+                        <div key="live">
+                            <LiveNowBadge className="fixed z-100 mt-2 ml-2" />
+                            <LiveNow show={data.liveNow} />
+                        </div>
+                    )}
+                    {(data?.upcoming && data.upcoming.length > 0) &&
                         <UpcomingShows shows={data.upcoming} isLive={Boolean(data.liveNow)} />
-                    </ScrollArea>
-                }
-                {(!data?.upcoming || data.upcoming.length === 0) && (
-                    <div key="empty" className="mx-4">
-                        <Alert data-is-live={Boolean(data?.liveNow)} className="mt-4">
-                            <AlertDescription>
-                                No upcoming shows scheduled at the moment.
-                            </AlertDescription>
-                        </Alert>
-                    </div>
-                )}
-            </AnimatePresence>
+                    }
+                    {(!data?.upcoming || data.upcoming.length === 0) && (
+                        <div key="empty" className="mx-4">
+                            <Alert data-is-live={Boolean(data?.liveNow)} className="mt-4">
+                                <AlertDescription>
+                                    No upcoming shows scheduled at the moment.
+                                </AlertDescription>
+                            </Alert>
+                        </div>
+                    )}
+                </AnimatePresence>
+            </ScrollArea>
         </Page>
     );
 }
